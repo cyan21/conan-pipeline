c702ec3e057c65e77eeafab1ec9e31e2b7e65460
cyan21 <yann.chaysinh@gmail.com>
2020-02-26 23:36:43 +0000

    test build info

--- scripts/generate_bi.sh
+++ scripts/generate_bi.sh
@@ -24,7 +24,7 @@ do
 done
 
 # set Build Info properties
-conan_build_info --v2 start $build_id $build_num
+#conan_build_info --v2 start $build_id $build_num
 
 # generate build info
 conan_build_info --v2 create --lockfile $lockfile_path/conan.lock --user $art_user --password $art_apikey ${output_file}
@@ -33,3 +33,4 @@ cat ${output_file}
 
 # remove Build Info props
 conan_build_info --v2 stop
+
