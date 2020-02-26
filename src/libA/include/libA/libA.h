65149ce81b3bb92a66c1163facf0fc9c1d210d21
cyan21 <yann.chaysinh@gmail.com>
2020-02-26 22:53:08 +0000

    removed src folder

--- src/libA/include/libA/libA.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-
-#include <string>
-
-void hello_libA(int indent, const std::string& msg="default");
--- src/libA/src/libA.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "libA/libA.h"
-
-#include <iostream>
-
-void hello_libA(int indent, const std::string& msg) {
-    std::cout << std::string(indent, ' ') << "libA: " << msg << std::endl;
-    std::cout << "version 1.0" << std::endl;
-}
